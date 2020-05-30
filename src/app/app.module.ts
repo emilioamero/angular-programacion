8458df188a857b25f5c6be1d66813fbb96c2499a
emilioamero <emilio.amero@gmail.com>
2020-05-30 17:35:10 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,20 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MenuComponent } from './menu/menu.component';
 import { PrincipalComponent } from './principal/principal.component';
+/*Enrutadores de Navegacion*/
+import { RouterModule, Route } from "@angular/router";
+
+
+
+
+/*Constantes de Rutas y Navegacion*/
+const routes: Route[] = [
+  {path: 'principal', component: PrincipalComponent},
+
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(routes) ],
   declarations: [ AppComponent, HelloComponent, MenuComponent, PrincipalComponent ],
   bootstrap:    [ AppComponent ]
 })
